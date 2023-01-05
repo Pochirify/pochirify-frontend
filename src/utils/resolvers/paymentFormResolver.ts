9e2a6f42b5034f2bf28ffa497c04b56752d76f06
kounosukexxx <dqx1005@icloud.com>
2023-01-06 01:02:36 +0900

    Revert "resolver"
    This reverts commit a05256589010203b5038ab5a464172bceca1b681.

--- src/utils/resolvers/paymentFormResolver.ts
+++
@@ -1,9 +0,0 @@
-import { yupResolver } from "@hookform/resolvers/yup";
-import * as yup from "yup";
-
-export const paymentFormResolver = yupResolver(
-  yup.object().shape({
-    address2: yup.string().min(1, "入力してください").max(10, "ddddd"),
-    address3: yup.string().min(1),
-  })
-);
